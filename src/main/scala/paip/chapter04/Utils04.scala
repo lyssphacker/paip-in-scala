5d3342c392cf9a68cc5b456627d206080781d104
Lyss P. Hacker <lyss.p.hacker@gmail.com>
2017-08-04 12:44:55 +0200

    Adding more debug utility functions.

--- src/main/scala/paip/chapter04/Utils04.scala
+++ src/main/scala/paip/chapter04/Utils04.scala
@@ -1,14 +1,32 @@
 package paip.chapter04
 
 object Utils04 {
+  var dbgIds: Set[String] = Set.empty
+
   implicit def intWithTimes(n: Int) = new {
     def times(f: => Unit) = 1 to n foreach { _ => f }
   }
 
-  def dbgIndent(indent: Int, str: String): Unit = {
-    indent times {
-      print(" ")
+  def dbgIndent(id: String, indent: Int, str: String): Unit = {
+    if (dbgIds.contains(id)) {
+      indent times {
+        print(" ")
+      }
+      println(str)
     }
-    println(str)
+  }
+
+  def dbg(id: String, str: String): Unit = {
+    if (dbgIds.contains(id)) {
+      println(str)
+    }
+  }
+
+  def debug(id: String): Unit = {
+    dbgIds = dbgIds + id
+  }
+
+  def undebug(id: String): Unit = {
+    dbgIds = dbgIds - id
   }
 }
