4d230635a30034ffdf8ffb78ac3c3efb29a8eeea
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-06 19:51:05 +0200

    Defining some of Eliza's rules.

--- src/main/scala/paip/chapter05/Eliza.scala
+++ src/main/scala/paip/chapter05/Eliza.scala
@@ -1,5 +1,20 @@
 package paip.chapter05
 
-object Eliza {
+import paip.chapter05.PatMatch.P
 
+object Eliza {
+  def Lhs = P
+
+  case class Rule(lhs: P, rhs: Rhs)
+
+  case class Rhs(values: List[P])
+
+  object Rhs {
+    def apply(values: String*) = new Rhs(values.toList.map((s: String) => P(s)))
+  }
+
+  implicit val elizaRules: List[Rule] = List(
+    Rule(Lhs("?*X hello ?*Y"), Rhs("How do you do. Please state your problem")),
+    Rule(Lhs("?*X I want ?*Y"), Rhs("What would it mean if you got ?Y", "Why do you want ?Y", "Suppose you got ?Y soon"))
+  )
 }
