7538b779217f5872e624743f047ac2dc67c2ec90
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-15 10:15:10 +0200

    Initial implementation of searchGps.

--- src/main/scala/paip/chapter06/GPSAsSearch.scala
+++ src/main/scala/paip/chapter06/GPSAsSearch.scala
@@ -1,5 +1,45 @@
 package paip.chapter06
 
-object GPSAsSearch {
+import paip.chapter04.BlocksWorld.makeBlockOps
+import paip.chapter04.FullGps.{convertOp, countIf, isAction}
+import paip.chapter04.Gps.Op
+import paip.chapter06.Search.{beamSearch, findAllIf}
 
+object GPSAsSearch {
+  implicit val convertedBlocksWorldOps = makeBlockOps(List("a", "b", "c")).map(convertOp)
+
+  def isSubset[T](lst1: List[T], lst2: List[T]): Boolean = {
+    lst1.forall(lst2.contains)
+  }
+
+  def applicableOps(state: List[String])(implicit ops: List[Op]): List[Op] = {
+    findAllIf[Op]((op: Op) => isSubset(op.preconds.toList, state), ops)
+  }
+
+  def gpsSuccessors(state: List[String]): List[List[String]] = {
+    applicableOps(state).map((op: Op) => {
+      val first = state.filter((s: String) => op.delList.contains(s))
+      val second = op.delList.toList
+      first ::: second
+    })
+  }
+
+  def searchGps(start: List[String], goal: List[String], beamWidth: Int = 10): List[String] = {
+    findAllIf[String](
+      isAction,
+      beamSearch[List[String], Int](
+        "start" :: start,
+        (state: List[String]) => state.forall(goal.contains),
+        gpsSuccessors,
+        (state: List[String]) => countIf(isAction, state) +
+          countIf((con: String) => !state.contains(con), goal),
+        beamWidth
+      ).get
+    )
+  }
+
+  def main(args: Array[String]): Unit = {
+    searchGps(List("c on a", "a on table", "b on table", "space on c",
+      "space on b", "space on table"), List("a on b", "space on table"))
+  }
 }
