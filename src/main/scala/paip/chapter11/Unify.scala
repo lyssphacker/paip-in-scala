221f2914ea8c73087a3632f1440b3cb4908b8c99
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-19 19:12:39 +0200

    Adding occursCheck.

--- src/main/scala/paip/chapter11/Unify.scala
+++ src/main/scala/paip/chapter11/Unify.scala
@@ -3,6 +3,8 @@ package paip.chapter11
 import paip.chapter05.PatMatchFacility._
 
 object Unify {
+  implicit val checkOccurs = true
+
   def unify(x: P, y: P, bindings: Bs = Bs.noBindings): Bs = {
     if (bindings.equals(Bs.fail)) Bs.fail
     else if (x.equals(y)) bindings
@@ -16,21 +18,32 @@ object Unify {
     else Bs.fail
   }
 
-  def unifyVariable(variable: String, x: P, bindings: Bs): Bs = {
+  def unifyVariable(variable: String, x: P, bindings: Bs)(implicit checkOccurs: Boolean): Bs = {
     if (bindings.getBinding(variable).isDefined)
       unify(P(bindings.lookup(variable)), x, bindings)
     else if (x.isVariable && bindings.getBinding(x.toString).isDefined)
       unify(P(variable), P(bindings.lookup(x.toString)), bindings)
+    else if (checkOccurs && occursCheck(variable, x, bindings)) Bs.fail
     else bindings.extendBindings(variable, x.toString)
+  }
+
+  def occursCheck(variable: String, x: P, bindings: Bs): Boolean = {
+    if (variable.equals(x.toString)) true
+    else if (x.isVariable && bindings.getBinding(x.toString).isDefined)
+      occursCheck(variable, P(bindings.lookup(x.toString)), bindings)
+    else if (x.isCons)
+      occursCheck(variable, x.first, bindings) || occursCheck(variable, x.rest, bindings)
+    else false
   }
 
   def main(args: Array[String]): Unit = {
 //    val result = unify(P("?x + 1"), P("2 + ?y"))
 //    val result = unify(P("?x"), P("?y"))
 //    val result = unify(P("?x ?x"), P("?y ?y"))
-//    val result = unify(P("?x ?x ?x"), P("?y ?y ?y"))
+    val result = unify(P("?x ?x ?x"), P("?y ?y ?y"))
 //    val result = unify(P("?x ?y"), P("?y ?x"))
-    val result = unify(P("?x ?y a"), P("?y ?x ?x"))
+//    val result = unify(P("?x ?y a"), P("?y ?x ?x"))
+//    val result = unify(P("?x"), P("f ?x"))
     result
   }
 }
