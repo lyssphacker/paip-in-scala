8b5e5369657f6fee3ddf14adfb86d54ae6b06fac
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-19 20:12:48 +0200

    Adding some functions.

--- src/main/scala/paip/chapter11/Unify.scala
+++ src/main/scala/paip/chapter11/Unify.scala
@@ -3,7 +3,7 @@ package paip.chapter11
 import paip.chapter05.PatMatchFacility._
 
 object Unify {
-  implicit val checkOccurs = true
+  implicit val checkOccurs = false
 
   def unify(x: P, y: P, bindings: Bs = Bs.noBindings): Bs = {
     if (bindings.equals(Bs.fail)) Bs.fail
@@ -36,14 +36,35 @@ object Unify {
     else false
   }
 
+  def substitute(bs: Bs, value: String): String = {
+    bs.bindings.foldLeft(value)((a, b) => a.replaceAllLiterally(b._1, b._2))
+  }
+
+  def substBindings(bindings: Bs, x: P): Option[P] = {
+    if (bindings.equals(Bs.fail)) None
+    else if (bindings.equals(Bs.noBindings)) Some(x)
+    else if (x.isVariable && bindings.getBinding(x.toString).isDefined)
+      substBindings(bindings, P(bindings.lookup(x.toString)))
+    else if (x.isAtom) Some(x)
+    else reuseCons(substBindings(bindings, x.first), substBindings(bindings, x.rest), x)
+  }
+
+  def reuseCons(x: Option[P], y: Option[P], xy: P): Option[P] = {
+    if (x.isEmpty || y.isEmpty) None
+    else if (x.get.equals(xy.first) && y.get.equals(xy.rest)) Some(xy)
+    else Some(P(x.get.value ::: y.get.value))
+  }
+
+  def unifier(x: P, y: P): Option[P] = {
+    substBindings(unify(x, y), x)
+  }
+
   def main(args: Array[String]): Unit = {
-//    val result = unify(P("?x + 1"), P("2 + ?y"))
-//    val result = unify(P("?x"), P("?y"))
-//    val result = unify(P("?x ?x"), P("?y ?y"))
-    val result = unify(P("?x ?x ?x"), P("?y ?y ?y"))
-//    val result = unify(P("?x ?y"), P("?y ?x"))
-//    val result = unify(P("?x ?y a"), P("?y ?x ?x"))
+//    val result = unify(P("a a"), P("a a"))
+//    val result = unifier(P("?x ?y a"), P("?y ?x ?x"))
+//    val result = unifier(P("(?a * ?x ^ 2) + (?b * ?x) + ?c"), P("?z + (4 * 5) + 3"))
 //    val result = unify(P("?x"), P("f ?x"))
+    val result = unify(P("?x ?y"), P("f ?y f ?x"))
     result
   }
 }
