1d4789130f7444ec4ab1b581fd2ac193de013434
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-20 15:51:14 +0200

    Finishing implementation of Prolog interpreter.

--- src/main/scala/paip/chapter11/Prolog1.scala
+++ src/main/scala/paip/chapter11/Prolog1.scala
@@ -1,15 +1,25 @@
 package paip.chapter11
 
+
+import paip.chapter05.PatMatchFacility.{Bs, P}
+import paip.chapter11.Unify._
+
+import scala.util.Random
+
 object Prolog1 {
   var dbPredicates: List[String] = List.empty
 
   var clausesMap: scala.collection.mutable.Map[String, List[C]] = scala.collection.mutable.Map.empty
 
-  case class C(head: R, body: Option[R]) {
-    def isAtom() = body.isDefined && body.get.value.nonEmpty
-  }
+  case class C(head: R, body: Option[R])
 
-  case class R(value: List[String])
+  case class R(value: List[String]) {
+    def isAtom = value.nonEmpty
+
+    def first: String = value.head
+
+    def rest: List[String] = value.tail
+  }
 
   object R {
     def apply(s: String) = R(s.split(" ").toList)
@@ -49,5 +59,66 @@ object Prolog1 {
 
   def clearDb(): Unit = {
     clausesMap.foreach(e => if (dbPredicates.contains(e._1)) clausesMap.remove(e._1))
+  }
+
+  def gensym(x: String): String = {
+    x + Random.nextInt(10000)
+  }
+
+  def proveGoals(goals: R*): Unit = {
+    showPrologSolutions(variablesIn(goals.toList), proveAll(goals.toList, Bs.noBindings))
+  }
+
+  def proveAll(goals: List[R], bindings: Bs): List[Bs] = {
+    if (bindings.equals(Bs.fail)) List(Bs.fail)
+    else if (goals.isEmpty) List(bindings)
+    else prove(goals.head, bindings).flatMap((bs: Bs) => proveAll(goals.tail, bs))
+  }
+
+  def prove(goal: R, bindings: Bs): List[Bs] = {
+    getClauses(predicate(goal)).get.flatMap((c: C) => {
+      val newClause = renameVariables(c)
+      proveAll(List(newClause.body.get), unify(P(goal.value), P(newClause.head.value), bindings))
+    })
+  }
+
+  def renameVariables(clause: C): C = {
+    val headBindings = Bs(variablesIn(List(clause.head)).map((v: String) => v -> gensym(v)).toMap)
+    val head = clause.head.value.map((s: String) => substitute(headBindings, s))
+    if (clause.body.isDefined) {
+      val bodyBindings = Bs(variablesIn(List(clause.body.get)).map((v: String) => v -> gensym(v)).toMap)
+      val body = clause.body.get.value.map((s: String) => substitute(bodyBindings, s))
+      C(R(head), Some(R(body)))
+    } else {
+      C(R(head), None)
+    }
+  }
+
+  def variablesIn(goals: List[R]): List[String] = {
+    goals.flatMap((g: R) => uniqueFindAnywhereif(isVariable, g))
+  }
+
+  def uniqueFindAnywhereif(predicate: String => Boolean, goal: R, foundSoFar: List[String] = List.empty): List[String] = {
+    if (goal.isAtom) {
+      if (predicate.apply(goal.first)) adjoin(goal.first, foundSoFar)
+      else foundSoFar
+    } else {
+      uniqueFindAnywhereif(predicate, R(goal.first), uniqueFindAnywhereif(predicate, R(goal.rest), foundSoFar))
+    }
+  }
+
+  def adjoin(item: String, lst: List[String]): List[String] = {
+    if (lst.contains(item)) lst
+    else item :: lst
+  }
+
+  def showPrologSolutions(vars: List[String], bindings: List[Bs]): Unit = {
+    if (bindings.isEmpty) println("No.")
+    else bindings.foreach((b: Bs) => showPrologVars(vars, b))
+  }
+
+  def showPrologVars(vars: List[String], bindings: Bs): Unit = {
+    if (vars.isEmpty) print("Yes")
+    else vars.foreach((v: String) => println(s"$v = ${substBindings(bindings, P(v)).get}"))
   }
 }
