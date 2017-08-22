8c2f32bfb3dbb3e29f72ef515e16350ae267fecf
Hrvoje Krot <hkrot@infoartgroup.com>
2017-08-22 17:03:22 +0200

    Implementing prove.

--- src/main/scala/paip/chapter11/Prolog.scala
+++ src/main/scala/paip/chapter11/Prolog.scala
@@ -4,6 +4,9 @@ import paip.chapter05.PatMatchFacility.Bs
 import paip.chapter11.Prolog1._
 
 object Prolog {
+
+  var showPrologVarsMap: scala.collection.mutable.Map[String, (List[String], Bs, List[R]) => Unit] = scala.collection.mutable.Map.empty
+
   def replaceAnonVars(goals: List[R]): List[R] = {
     goals.map((g: R) => replaceAnonVar(g))
   }
@@ -22,7 +25,11 @@ object Prolog {
 
   def prove(goal: R, bindings: Bs, otherGoals: List[R]): Bs = {
     val clauses = getClauses(predicate(goal))
-    if (clauses.get.nonEmpty)
+    if (clauses.get.nonEmpty) {
+
+    } else {
+      showPrologVarsMap.get(predicate(goal)).get.apply(goal.rest, bindings, otherGoals)
+    }
   }
 
   def proveAll(goals: List[R], bindings: Bs): Bs = {
@@ -33,5 +40,13 @@ object Prolog {
 
   def proveGoals(goals: R*): Unit = {
 
+  }
+
+  def showPrologVars(vars: List[String], bindings: Bs, otherGoals: List[R]): Unit = {
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    showPrologVarsMap.put("showPrologVars", showPrologVars _)
   }
 }
