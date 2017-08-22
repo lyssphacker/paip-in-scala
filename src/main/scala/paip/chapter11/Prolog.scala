b1907969b93da05838221bf5e7ddaeaf9ad42c62
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-22 19:40:00 +0200

    Finishing implementation of Prolog interpreter.

--- src/main/scala/paip/chapter11/Prolog.scala
+++ src/main/scala/paip/chapter11/Prolog.scala
@@ -1,11 +1,12 @@
 package paip.chapter11
 
-import paip.chapter05.PatMatchFacility.Bs
-import paip.chapter11.Prolog1._
+import paip.chapter05.PatMatchFacility.{Bs, P}
+import paip.chapter11.Prolog1.{addClauses, proveGoals, _}
+import paip.chapter11.Unify._
 
 object Prolog {
 
-  var showPrologVarsMap: scala.collection.mutable.Map[String, (List[String], Bs, List[R]) => Unit] = scala.collection.mutable.Map.empty
+  var showPrologVarsMap: scala.collection.mutable.Map[String, (List[String], Bs, List[R]) => Bs] = scala.collection.mutable.Map.empty
 
   def replaceAnonVars(goals: List[R]): List[R] = {
     goals.map((g: R) => replaceAnonVar(g))
@@ -24,11 +25,18 @@ object Prolog {
   }
 
   def prove(goal: R, bindings: Bs, otherGoals: List[R]): Bs = {
-    val clauses = getClauses(predicate(goal))
-    if (clauses.get.nonEmpty) {
-
+    val clauses: Option[List[C]] = getClauses(predicate(goal))
+    if (clauses.isDefined) {
+      var result = Bs.fail
+      clauses.get.find((c: C) => {
+        val newClause = renameVariables(c)
+        result = proveAll(otherGoals ::: newClause.body,
+          unify(P(goal.value), P(newClause.head.value), bindings))
+        !result.equals(Bs.fail)
+      })
+      result
     } else {
-      showPrologVarsMap.get(predicate(goal)).get.apply(goal.rest, bindings, otherGoals)
+      showPrologVarsMap(predicate(goal)).apply(goal.rest, bindings, otherGoals)
     }
   }
 
@@ -39,14 +47,56 @@ object Prolog {
   }
 
   def proveGoals(goals: R*): Unit = {
-
+    proveAll(goals.toList :+ createshowPrologVarsR(goals.toList), Bs.noBindings)
+    println("No.")
   }
 
-  def showPrologVars(vars: List[String], bindings: Bs, otherGoals: List[R]): Unit = {
+  def createshowPrologVarsR(goals: List[R]): R = {
+    val vars = variablesIn(goals, isNonAnonVariable)
+    R("showPrologVars " + vars.mkString(" "))
+  }
 
+  def isNonAnonVariable(variable: String): Boolean = {
+    isVariable(variable) && !variable.equals("?")
+  }
+
+  def showPrologVars(vars: List[String], bindings: Bs, otherGoals: List[R]): Bs = {
+    if (vars.isEmpty) println("Yes.")
+    else {
+      vars.foreach((v: String) => println(s"$v = ${substBindings(bindings, P(v))}"))
+    }
+
+    if (continue()) Bs.fail
+    else proveAll(otherGoals, bindings)
+  }
+
+  def continue(): Boolean = {
+    val input = scala.io.StdIn.readChar()
+    input match {
+      case ';' => true
+      case '.' => false
+      case '-' => continue()
+      case _ => {
+        println("Type ; to see more or . to stop")
+        continue()
+      }
+    }
   }
 
   def main(args: Array[String]): Unit = {
     showPrologVarsMap.put("showPrologVars", showPrologVars _)
+
+    addClauses(C(R("likes Kim Robin")))
+    addClauses(C(R("likes Sandy Lee")))
+    addClauses(C(R("likes Sandy Kim")))
+    addClauses(C(R("likes Robin cats")))
+    addClauses(C(R("likes Sandy ?x"), R("likes ?x cats")))
+    addClauses(C(R("likes Sandy ?x"), R("likes ?x cats"), R("likes ?x Kim")))
+    addClauses(C(R("likes ?x ?x")))
+
+        proveGoals(R("likes Sandy ?who"))
+//    proveGoals(R("likes ?who Sandy"))
+    //    proveGoals(R("likes Robin Lee"))
+    //    proveGoals(R("likes ?x ?y"), R("likes ?y ?x"))
   }
 }
