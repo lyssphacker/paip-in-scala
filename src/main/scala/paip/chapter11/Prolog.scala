bff48b548a583cd89276b2627cc16e41f6ee87a5
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-22 05:32:08 +0200

    Adding replaceAnonVars.

--- src/main/scala/paip/chapter11/Prolog.scala
+++ src/main/scala/paip/chapter11/Prolog.scala
@@ -1,5 +1,21 @@
 package paip.chapter11
 
-object Prolog {
+import paip.chapter11.Prolog1._
 
+object Prolog {
+  def replaceAnonVars(goals: List[R]): List[R] = {
+    goals.map((g: R) => replaceAnonVar(g))
+  }
+
+  def replaceAnonVar(goal: R): R = {
+    R(goal.value.mkString(" ").replace("?", gensym("?")))
+  }
+
+  def replaceAnonVars(clause: C): C = {
+    C(replaceAnonVar(clause.head), replaceAnonVars(clause.body))
+  }
+
+  def addClauses(clauses: C*): Unit = {
+    for (clause <- clauses) addClause(replaceAnonVars(clause))
+  }
 }
