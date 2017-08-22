436aa1e5726ef56f46ecaf8397aebf8f5fbfa70a
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-22 06:07:56 +0200

    Initial implementation of prove and proveAll.

--- src/main/scala/paip/chapter11/Prolog.scala
+++ src/main/scala/paip/chapter11/Prolog.scala
@@ -1,5 +1,6 @@
 package paip.chapter11
 
+import paip.chapter05.PatMatchFacility.Bs
 import paip.chapter11.Prolog1._
 
 object Prolog {
@@ -17,5 +18,16 @@ object Prolog {
 
   def addClauses(clauses: C*): Unit = {
     for (clause <- clauses) addClause(replaceAnonVars(clause))
+  }
+
+  def prove(goal: R, bindings: Bs, otherGoals: List[R]): Bs = {
+    val clauses = getClauses(predicate(goal))
+    if (clauses.get.nonEmpty)
+  }
+
+  def proveAll(goals: List[R], bindings: Bs): Bs = {
+    if (bindings.equals(Bs.fail)) Bs.fail
+    else if (goals.isEmpty) bindings
+    else prove(goals.head, bindings, goals.tail)
   }
 }
