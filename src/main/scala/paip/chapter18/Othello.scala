557e5e1bd05483437e1daf5e6352ad13acb1def6
Hrvoje Krot <hrvoje.krot@gmail.com>
2017-08-23 18:43:30 +0200

    Making progress on Othello.

--- src/main/scala/paip/chapter18/Othello.scala
+++ src/main/scala/paip/chapter18/Othello.scala
@@ -10,4 +10,30 @@ object Othello {
     val outer = Value("?")
   }
 
+  val allDirections = List(-11, -10, -9, -1, 1, 9, 10, 11)
+
+  import paip.chapter18.Othello.Piece._
+
+  def opponent(player: Piece): Piece = {
+    if (player.equals(black)) white
+    else black
+  }
+
+  case class Board() {
+    val arr: Array[Piece] = new Array(100)
+
+    def aref(square: Int): Piece = {
+      arr(square)
+    }
+
+    def aset(square: Int, value: Piece): Unit = {
+      arr(square) = value
+    }
+
+  }
+
+  def main(args: Array[String]): Unit = {
+    val board = Board()
+    board.arr
+  }
 }
