1586b3d2f1a28b14341607683f802b905fc5f1b3
Hrvoje Krot <hkrot@infoartgroup.com>
2017-08-29 16:21:01 +0200

    Initializing allSquares.

--- src/main/scala/paip/chapter18/Othello2.scala
+++ src/main/scala/paip/chapter18/Othello2.scala
@@ -1,5 +1,10 @@
 package paip.chapter18
 
-object Othello2 {
+import paip.chapter18.Othello.weights
 
+object Othello2 {
+  val allSquares: List[Int] = (11 to 88).filter((i: Int) => {
+    val mod = i % 10
+    mod >= 1 && mod <= 8
+  }).toList.sortWith(weights(_) > weights(_))
 }
