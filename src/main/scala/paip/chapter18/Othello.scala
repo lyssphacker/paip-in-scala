d43aa733856720e8a421cecad9bd04387e52072b
Hrvoje Krot <hkrot@infoartgroup.com>
2017-10-03 07:57:24 +0200

    Fixing call to randomOthelloSeries.

--- src/main/scala/paip/chapter18/Othello.scala
+++ src/main/scala/paip/chapter18/Othello.scala
@@ -608,15 +608,15 @@ object Othello {
 //    othello(minimaxSearcher(3, adaptEvalFn(countDifference)), adaptStrategy(maximizier(countDifference)))
 //    othello(adaptStrategy(maximizier(weightedSquares)), adaptStrategy(maximizier(countDifference)))
 //        othello(alphaBetaSearcher(6, adaptEvalFn(countDifference)), alphaBetaSearcher(4, adaptEvalFn(weightedSquares)))
-    //    val result = randomOthelloSeries(
-    //      alphaBetaSearcher(2, adaptEvalFn(weightedSquares)),
-    //      alphaBetaSearcher(2, adaptEvalFn(modifiedWeightedSquares)),
-    //      5)
-    //    result
-        roundRobin(
-          List(adaptStrategy(maximizier(countDifference)), adaptStrategy(maximizier(mobility)),
-            adaptStrategy(maximizier(weightedSquares)), adaptStrategy(maximizier(modifiedWeightedSquares)), randomStrategy), 5, 10,
-          List("count-difference", "mobility", "weighted", "modified-weighted", "random"))
+        val result = randomOthelloSeries(
+          alphaBetaSearcher(4, adaptEvalFn(weightedSquares)),
+          randomStrategy,
+          5)
+        result
+//        roundRobin(
+//          List(adaptStrategy(maximizier(countDifference)), adaptStrategy(maximizier(mobility)),
+//            adaptStrategy(maximizier(weightedSquares)), adaptStrategy(maximizier(modifiedWeightedSquares)), randomStrategy), 5, 10,
+//          List("count-difference", "mobility", "weighted", "modified-weighted", "random"))
 
 //    roundRobin(
 //          List(alphaBetaSearcher(4, adaptEvalFn(countDifference)), alphaBetaSearcher(4, adaptEvalFn(weightedSquares)),
